1a3ab6b5cba76893034d07c1066161b6e7f3dd49
pedro-donoso <pedro.eduardo.donoso@gmail.com>
2025-11-08 18:02:15 -0300

    agrego models

--- recetas/models.py
+++ recetas/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Receta(models.Model):
+    nombre = models.CharField(max_length=200)
+    ingredientes = models.TextField()
+    instrucciones = models.TextField()
+    imagen = models.ImageField(upload_to='recetas/')
+    
+    def __str__(self):
+        return self.nombre
